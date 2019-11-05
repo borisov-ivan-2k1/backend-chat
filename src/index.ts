74583cd60e8c6586fc0a0fffae3448ba6b153bcf
borisov-ivan-2k1 <borisov.ivan.2001.rkn@gmail,ru>
2019-11-05 15:09:59 +0300

    made authorization, create / delete user, dialogs and messages

---
+++ src/index.ts
@@ -0,0 +1,46 @@
+import mongoose from "mongoose";
+import express from "express";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+
+import {
+  UserController,
+  DialogController,
+  MessageController
+} from "./controllers";
+
+import { updateLastSeen, checkAuth } from "./middlewares";
+
+const app = express();
+dotenv.config();
+
+app.use(bodyParser.json());
+app.use(updateLastSeen);
+app.use(checkAuth);
+
+const User = new UserController();
+const Dialog = new DialogController();
+const Messages = new MessageController();
+
+mongoose.connect("mongodb://localhost:27017/chat", {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+});
+
+app.get("/user/:id", User.show);
+app.delete("/user/:id", User.delete);
+app.post("/user/registration", User.create);
+app.post("/user/login", User.login);
+
+app.get("/dialogs", Dialog.index);
+app.delete("/dialogs/:id", Dialog.delete);
+app.post("/dialogs", Dialog.create);
+
+app.get("/messages", Messages.index);
+app.post("/messages", Messages.create);
+app.delete("/messages/:id", Messages.delete);
+
+app.listen(process.env.PORT, function() {
+  console.log(`Server: http://localhost:${process.env.PORT}`);
+});
